a5c70b4c29dc7dd99c895647218edd54380b55c9
Monil Jain <jainmonil2101@gmail.com>
2021-06-26 18:31:36 +0530

    Tweaked static

--- dentist/settings.py
+++ dentist/settings.py
@@ -27,10 +27,10 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-# ALLOWED_HOSTS = ['127.0.0.1', 'django-dento.herokuapp.com']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'django-dento.herokuapp.com']
+
 
 
 # Application definition
